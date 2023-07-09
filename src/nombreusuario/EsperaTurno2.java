54a8d0abddf43d41c13b09d9d411daa99b69aa62
JonnyalReyes <jonathanalexi503y507@gmail.com>
2023-07-08 21:15:27 -0500

    Hola

--- src/nombreusuario/EsperaTurno2.java
+++ src/nombreusuario/EsperaTurno2.java
@@ -6,39 +6,48 @@ package nombreusuario;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JLabel;
 import javax.swing.Timer;
 
 /**
  *
  * @author kenet
  */
-public class EsperaTurno extends javax.swing.JFrame {
+public class EsperaTurno2 extends javax.swing.JFrame {
 
-    /**
-     * Creates new form EsperaTurno
-     */
-    public EsperaTurno() {
+  
+    Player1 nose = new Player1();
+    
+    public static JLabel Player2 = new JLabel();
+    
+    
+    public EsperaTurno2() {
         initComponents();
-       int delay = 6000; // Tiempo de espera en milisegundos (6 segundos)
-        ActionListener taskPerformer = new ActionListener() {
-        public void actionPerformed(ActionEvent evt) {
-            Player2 player2 = new Player2();
-            player2.setVisible(true);
-            setVisible(false);
-        }
-    };
-    Timer timer = new Timer(delay, taskPerformer);
-    timer.setRepeats(false);
-    timer.start();
+       addWindowListener(new WindowAdapter() {
+            public void windowActivated(WindowEvent e) {
+                Timer timer = new Timer(6000, new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    nose.setVisible(true);
+                    EsperaTurno2.this.setVisible(false);
+                    
+                }
+            });
+            timer.setRepeats(false);
+            timer.start();
+         
+            }
+        });
     }
-
+  
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         Panelespera = new javax.swing.JPanel();
         jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        nombre2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setLocationByPlatform(true);
@@ -49,10 +58,9 @@ public class EsperaTurno extends javax.swing.JFrame {
 
         jPanel1.setBackground(new java.awt.Color(0, 0, 0));
 
-        jLabel1.setFont(new java.awt.Font("Roboto Black", 0, 48)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("CAMBIANDO DE TURNO");
+        nombre2.setFont(new java.awt.Font("Roboto Black", 0, 48)); // NOI18N
+        nombre2.setForeground(new java.awt.Color(255, 255, 255));
+        nombre2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -60,14 +68,14 @@ public class EsperaTurno extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 788, Short.MAX_VALUE)
+                .addComponent(nombre2, javax.swing.GroupLayout.DEFAULT_SIZE, 788, Short.MAX_VALUE)
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 254, Short.MAX_VALUE)
+                .addComponent(nombre2, javax.swing.GroupLayout.DEFAULT_SIZE, 254, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -87,7 +95,7 @@ public class EsperaTurno extends javax.swing.JFrame {
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
-
+    
     /**
      * @param args the command line arguments
      */
@@ -105,27 +113,29 @@ public class EsperaTurno extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(EsperaTurno.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(EsperaTurno2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(EsperaTurno.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(EsperaTurno2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(EsperaTurno.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(EsperaTurno2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(EsperaTurno.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(EsperaTurno2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new EsperaTurno().setVisible(true);
+                new EsperaTurno2().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel Panelespera;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
+    public static javax.swing.JLabel nombre2;
     // End of variables declaration//GEN-END:variables
+
 }
